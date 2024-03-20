64cb260bfc5a72dee1e5bf3aab37a9b41b400100
이준형 <gaejungrhee@ijunhyeong-ui-MacBookPro.local>
2024-03-19 19:53:12 -0500

    최종본 3차

--- MosaicSphere/MosaicSphere/settings.py
+++ MosaicSphere/MosaicSphere/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l(r)mwh9k5#0@zbdm28!*0ijhic20s!72*kb+9z80u5!1i%bd-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
